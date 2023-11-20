fbb1742bbe0c27f1af97bb8724044dcb08129182
Aimeos <aimeos@aimeos.org>
2023-11-20 14:40:25 +0100

    Migrate brand images

--- setup/WooMigrateBrands.php
+++ setup/WooMigrateBrands.php
@@ -27,23 +27,33 @@ class WooMigrateBrands extends Base
 
 		$this->info( 'Migrate WooCommerce brands', 'vv' );
 
-		$textManager = \Aimeos\MShop::create( $this->context(), 'text' );
-		$manager = \Aimeos\MShop::create( $this->context(), 'supplier' );
+		$context = $this->context();
+		$mediaManager = \Aimeos\MShop::create( $context, 'media' );
+		$textManager = \Aimeos\MShop::create( $context, 'text' );
+		$manager = \Aimeos\MShop::create( $context, 'supplier' );
 		$brands = $manager->search( $manager->filter()->slice( 0, 0x7fffffff ), ['text'] );
 
-		$langId = $this->context()->locale()->getLanguageId();
+		$langId = $context->locale()->getLanguageId();
 
 		$db2 = $this->db( 'db-supplier' );
 
 		$result = $db->query( "
 			SELECT
-				t.term_id, t.name, t.slug, tt.description,
+				t.term_id,
+				t.name,
+				t.slug,
+				tt.description,
 				st.meta_value AS metatitle,
-				sd.meta_value AS metadesc
+				sd.meta_value AS metadesc,
+				pi.guid AS imgurl,
+				pi.post_title AS imglabel,
+				pi.post_mime_type AS mimetype
 			FROM wp_terms t
 			JOIN wp_term_taxonomy tt ON t.term_id=tt.term_id
 			LEFT JOIN wp_termmeta st ON st.term_id=t.term_id AND st.meta_key='_seopress_titles_title'
 			LEFT JOIN wp_termmeta sd ON sd.term_id=t.term_id AND sd.meta_key='_seopress_titles_desc'
+			LEFT JOIN wp_termmeta si ON si.term_id=t.term_id AND si.meta_key='thumbnail_id'
+			LEFT JOIN wp_posts pi ON pi.ID=si.meta_value
 			WHERE taxonomy='brand'
 			ORDER BY t.name
 		" );
@@ -83,6 +93,14 @@ class WooMigrateBrands extends Base
 				$refItem = $listItem->getRefItem() ?: $textManager->create();
 				$refItem->setType( 'meta-description' )->setLanguageId( $langId )->setContent( $text );
 				$item->addListItem( 'text', $listItem, $refItem->setLabel( mb_substr( strip_tags( $text ), 0, 60 ) ) );
+			}
+
+			if( $image = $row['imgurl'] ?? null )
+			{
+				$listItem = $item->getListItems( 'media', 'default', 'default' )->first() ?? $manager->createListItem();
+				$refItem = $listItem->getRefItem() ?: $mediaManager->create();
+				$refItem->setType( 'default' )->setUrl( $image )->setMimetype( $row['mimetype'] ?? '' );
+				$item->addListItem( 'media', $listItem, $refItem->setLabel( $row['imglabel'] ?? $image ) );
 			}
 
 			$brands[$item->getId()] = $manager->save( $item );
